c770e97402ea18829e4a014f1afecd631fa4925f
seaman <talaypan11@gmail.com>
2026-06-21 15:26:22 +0700

    feat: add font and color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Geist, Geist_Mono, Noto_Sans, JetBrains_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Noto_Sans, JetBrains_Mono, Source_Code_Pro, IBM_Plex_Sans_Thai } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Footer } from "@/components/ui/footer";
+import { GridPattern } from "@/components/ui/grid-pattern";
 
 const jetbrainsMonoHeading = JetBrains_Mono({
   subsets: ["latin"],
@@ -21,6 +23,17 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const sourceCodePro = Source_Code_Pro({
+  subsets: ["latin"],
+  variable: "--font-source-code-pro",
+});
+
+const ibmPlexSansThai = IBM_Plex_Sans_Thai({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["thai", "latin"],
+  variable: "--font-ibm-plex-sans-thai",
+});
+
 export const metadata: Metadata = {
   title: "Chem Bonding",
   description: "ใส่รหัสนักศึกษาเพื่อดูคำใบ้",
@@ -35,25 +48,39 @@ export default function RootLayout({
     <html
       lang="en"
       className={cn(
-        "dark",
         "h-full",
         "antialiased",
         geistSans.variable,
         geistMono.variable,
-        "font-sans",
+        "font-thai",
         notoSans.variable,
         jetbrainsMonoHeading.variable,
+        sourceCodePro.variable,
+        ibmPlexSansThai.variable,
       )}
       suppressHydrationWarning
     >
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
-          <main className="relative z-10 h-full w-full">{children}</main>
+          <main className="relative">
+            <GridPattern
+                    width={30}
+                    height={30}
+                    x={0}
+                    y={0}
+                    className="stroke-muted-foreground/20"
+                  />
+            <div className="flex-1 flex h-full w-full mx-auto min-h-screen max-w-3xl flex-col px-4 py-6 sm:px-6 sm:py-10">
+              {children}
+
+              <Footer />
+            </div>
+          </main>
         </ThemeProvider>
       </body>
     </html>
